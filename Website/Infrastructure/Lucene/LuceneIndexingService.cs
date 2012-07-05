2944b5f35cfc97b37a613c2b5bf6e0d61019067a
Raquel Almeida <raquel_soares@msn.com>
2012-07-05 10:42:31 -0700

    Changing Nuget.exe by Nuget bootstrapper

---
+++ Website/Infrastructure/Lucene/LuceneIndexingService.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+
+namespace NuGetGallery
+{
+    public class LuceneIndexingService : IIndexingService
+    {
+        private static readonly TimeSpan indexRecreateTime = TimeSpan.FromDays(3);
+        private static readonly char[] idSeparators = new[] { '.', '-' };
+
+        public void UpdateIndex()
+        {
+            DateTime? createdTime = GetIndexCreationTime();
+            if (createdTime.HasValue && (DateTime.UtcNow - createdTime > indexRecreateTime))
+            {
+                ClearLuceneDirectory();
+            }
+
+            DateTime? lastWriteTime = GetLastWriteTime();
+            bool creatingIndex = lastWriteTime == null;
+            using (var context = CreateContext())
+            {
+                var packages = GetPackages(context, lastWriteTime);
+                if (packages.Any())
+                {
+                    EnsureIndexDirectory();
+                    WriteIndex(creatingIndex, packages);
+                }
+            }
+            UpdateLastWriteTime();
+        }
+
+        protected internal virtual DbContext CreateContext()
+        {
+            return new EntitiesContext();
+        }
+
+        protected internal virtual List<PackageIndexEntity> GetPackages(DbContext context, DateTime? dateTime)
+        {
+            if (dateTime == null)
+            {
+                // If we're creating the index for the first time, fetch the new packages.
+                string sql = @"Select p.[Key], pr.Id, p.Title, p.Description, p.Tags, p.FlattenedAuthors as Authors, pr.DownloadCount, p.[Key] as LatestKey
+                         from Packages p join PackageRegistrations pr on p.PackageRegistrationKey = pr.[Key]
+                         where p.IsLatestStable = 1 or (p.IsLatest = 1 and Not exists (Select 1 from Packages iP where iP.PackageRegistrationKey = p.PackageRegistrationKey and iP.IsLatestStable = 1))";
+                return context.Database.SqlQuery<PackageIndexEntity>(sql).ToList();
+            }
+            else
+            {
+                string sql = @"Select p.[Key], pr.Id, p.Title, p.Description, p.Tags, p.FlattenedAuthors as Authors, pr.DownloadCount,
+                                   LatestKey = CASE When p.IsLatest = 1 then p.[Key] Else (Select pLatest.[Key] from Packages pLatest where pLatest.PackageRegistrationKey = pr.[Key] and pLatest.IsLatest = 1) End
+                                   from Packages p join PackageRegistrations pr on p.PackageRegistrationKey = pr.[Key]
+                                   where p.LastUpdated > @UpdatedDate";
+                return context.Database.SqlQuery<PackageIndexEntity>(sql, new SqlParameter("UpdatedDate", dateTime.Value)).ToList();
+            }
+        }
+
+        protected internal virtual void WriteIndex(bool creatingIndex, List<PackageIndexEntity> packages)
+        {
+            using (var directory = new LuceneFileSystem(LuceneCommon.IndexDirectory))
+            {
+                var analyzer = new StandardAnalyzer(LuceneCommon.LuceneVersion);
+                var indexWriter = new IndexWriter(directory, analyzer, create: creatingIndex, mfl: IndexWriter.MaxFieldLength.UNLIMITED);
+                AddPackages(indexWriter, packages);
+                indexWriter.Close();
+            }
+        }
+
+        private static void AddPackages(IndexWriter indexWriter, List<PackageIndexEntity> packages)
+        {
+            foreach (var package in packages)
+            {
+                if (package.Key != package.LatestKey)
+                {
+                    indexWriter.DeleteDocuments(new TermQuery(new Term("Key", package.Key.ToString(CultureInfo.InvariantCulture))));
+                    continue;
+                }
+
+                // If there's an older entry for this package, remove it.
+                var document = new Document();
+
+                document.Add(new Field("Key", package.Key.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
+                document.Add(new Field("Id-Exact", package.Id, Field.Store.NO, Field.Index.ANALYZED));
+
+                document.Add(new Field("Description", package.Description, Field.Store.NO, Field.Index.ANALYZED));
+
+                foreach (var idToken in TokenizeId(package.Id))
+                {
+                    document.Add(new Field("Id", idToken, Field.Store.NO, Field.Index.ANALYZED));
+                }
+
+                if (!String.IsNullOrEmpty(package.Title))
+                {
+                    document.Add(new Field("Title", package.Title, Field.Store.NO, Field.Index.ANALYZED));
+                }
+                if (!String.IsNullOrEmpty(package.Tags))
+                {
+                    document.Add(new Field("Tags", package.Tags, Field.Store.NO, Field.Index.ANALYZED));
+                }
+                document.Add(new Field("Author", package.Authors, Field.Store.NO, Field.Index.ANALYZED));
+                document.Add(new Field("DownloadCount", package.DownloadCount.ToString(CultureInfo.InvariantCulture), Field.Store.NO, Field.Index.ANALYZED_NO_NORMS));
+
+                indexWriter.AddDocument(document);
+            }
+        }
+
+        protected internal virtual void EnsureIndexDirectory()
+        {
+            if (!Directory.Exists(LuceneCommon.IndexDirectory))
+            {
+                Directory.CreateDirectory(LuceneCommon.IndexDirectory);
+            }
+        }
+
+        protected internal virtual DateTime? GetIndexCreationTime()
+        {
+            if (File.Exists(LuceneCommon.IndexMetadataPath))
+            {
+                var text = File.ReadLines(LuceneCommon.IndexMetadataPath).FirstOrDefault();
+                DateTime dateTime;
+                if (!String.IsNullOrEmpty(text) && DateTime.TryParseExact(text, "R", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime))
+                {
+                    return dateTime;
+                }
+            }
+            
+            return null;
+        }
+
+        protected internal virtual void ClearLuceneDirectory()
+        {
+            if (Directory.Exists(LuceneCommon.IndexDirectory))
+            {
+                Directory.Delete(LuceneCommon.IndexDirectory, recursive: true);
+            }
+        }
+
+        protected internal virtual DateTime? GetLastWriteTime()
+        {
+            if (!File.Exists(LuceneCommon.IndexMetadataPath))
+            {
+                return null;
+            }
+            return File.GetLastWriteTimeUtc(LuceneCommon.IndexMetadataPath);
+        }
+
+        protected internal virtual void UpdateLastWriteTime()
+        {
+            if (!File.Exists(LuceneCommon.IndexMetadataPath))
+            {
+                if (Directory.Exists(LuceneCommon.IndexMetadataPath))
+                {
+                    // If the directoey exists, then assume that the index has been created.
+                    File.WriteAllText(LuceneCommon.IndexMetadataPath, DateTime.UtcNow.ToString("R"));
+                }
+            }
+            else
+            {
+                File.SetLastWriteTimeUtc(LuceneCommon.IndexMetadataPath, DateTime.UtcNow);
+            }
+        }
+
+        internal static IEnumerable<string> TokenizeId(string term)
+        {
+            var result = CamelCaseTokenize(term).SelectMany(s => s.Split(idSeparators, StringSplitOptions.RemoveEmptyEntries)).ToList();
+            if (result.Count == 1)
+            {
+                return Enumerable.Empty<string>();
+            }
+            return result;
+        }
+
+        private static IEnumerable<string> CamelCaseTokenize(string term)
+        {
+            if (term.Length < 2)
+            {
+                yield break;
+            }
+
+            int tokenStart = 0;
+            for (int i = 1; i < term.Length; i++)
+            {
+                if (Char.IsUpper(term[i]) && (i - tokenStart > 2))
+                {
+                    yield return term.Substring(tokenStart, i - tokenStart);
+                    tokenStart = i;
+                }
+            }
+            if (term.Length - tokenStart < 2)
+            {
+                yield break;
+            }
+            yield return term.Substring(tokenStart);
+        }
+    }
+}
---
+++ Website/Infrastructure/Lucene/LuceneSearchService.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Index;
+
+namespace NuGetGallery
+{
+    public class LuceneSearchService : ISearchService
+    {
+        private const int MaximumRecordsToReturn = 1000;
+
+        public IQueryable<Package> Search(IQueryable<Package> packages, string searchTerm)
+        {
+            if (String.IsNullOrEmpty(searchTerm))
+            {
+                return packages;
+            }
+            var keys = SearchCore(searchTerm);
+            return SearchByKeys(packages, keys);
+        }
+
+        public IQueryable<Package> SearchWithRelevance(IQueryable<Package> packages, string searchTerm)
+        {
+            int numberOfHits;
+            return SearchWithRelevance(packages, searchTerm, MaximumRecordsToReturn, out numberOfHits);
+        }
+
+        public IQueryable<Package> SearchWithRelevance(IQueryable<Package> packages, string searchTerm, int take, out int numberOfHits)
+        {
+            numberOfHits = 0;
+            if (String.IsNullOrEmpty(searchTerm))
+            {
+                return packages;
+            }
+
+            var keys = SearchCore(searchTerm);
+            if (!keys.Any())
+            {
+                return Enumerable.Empty<Package>().AsQueryable();
+            }
+
+            numberOfHits = keys.Count();
+            var results = SearchByKeys(packages, keys.Take(take));
+            var lookup = results.ToDictionary(p => p.Key, p => p);
+
+            return keys.Select(key => LookupPackage(lookup, key))
+                       .Where(p => p != null)
+                       .AsQueryable();
+        }
+
+        private static Package LookupPackage(Dictionary<int, Package> dict, int key)
+        {
+            Package package;
+            dict.TryGetValue(key, out package);
+            return package;
+        }
+
+        private static IQueryable<Package> SearchByKeys(IQueryable<Package> packages, IEnumerable<int> keys)
+        {
+            return packages.Where(p => keys.Contains(p.Key));
+        }
+
+        private static IEnumerable<int> SearchCore(string searchTerm)
+        {
+            if (!Directory.Exists(LuceneCommon.IndexDirectory))
+            {
+                return Enumerable.Empty<int>();
+            }
+
+            using (var directory = new LuceneFileSystem(LuceneCommon.IndexDirectory))
+            {
+                var searcher = new IndexSearcher(directory, readOnly: true);
+                var query = ParseQuery(searchTerm);
+                var results = searcher.Search(query, filter: null, n: 1000, sort: new Sort(new[] { SortField.FIELD_SCORE, new SortField("DownloadCount", SortField.INT, reverse: true) }));
+                var keys = results.scoreDocs.Select(c => Int32.Parse(searcher.Doc(c.doc).Get("Key"), CultureInfo.InvariantCulture))
+                                            .ToList();
+                searcher.Close();
+                return keys;
+            }
+        }
+
+        private static Query ParseQuery(string searchTerm)
+        {
+            var fields = new Dictionary<string, float> { { "Id", 1.2f }, { "Title", 1.0f }, { "Tags", 1.0f}, { "Description", 0.8f }, { "Author", 0.6f } };
+            var analyzer = new StandardAnalyzer(LuceneCommon.LuceneVersion);
+            searchTerm = QueryParser.Escape(searchTerm).ToLowerInvariant();
+
+            var queryParser = new MultiFieldQueryParser(LuceneCommon.LuceneVersion, fields.Keys.ToArray(), analyzer, fields);
+
+            var conjuctionQuery = new BooleanQuery();
+            conjuctionQuery.SetBoost(1.5f);
+            var disjunctionQuery = new BooleanQuery();
+            var wildCardQuery = new BooleanQuery();
+            wildCardQuery.SetBoost(0.7f);
+            var exactIdQuery = new TermQuery(new Term("Id-Exact", searchTerm));
+            exactIdQuery.SetBoost(2.5f);
+            
+            foreach(var term in searchTerm.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                conjuctionQuery.Add(queryParser.Parse(term), BooleanClause.Occur.MUST);
+                disjunctionQuery.Add(queryParser.Parse(term), BooleanClause.Occur.SHOULD);
+                
+                foreach (var field in fields)
+                {
+                    var wildCardTermQuery = new WildcardQuery(new Term(field.Key, term + "*"));
+                    wildCardTermQuery.SetBoost(0.7f * field.Value);
+                    wildCardQuery.Add(wildCardTermQuery, BooleanClause.Occur.SHOULD);
+                }
+            }
+
+            return conjuctionQuery.Combine(new Query[] { exactIdQuery, conjuctionQuery, disjunctionQuery, wildCardQuery });
+        }
+    }
+}
